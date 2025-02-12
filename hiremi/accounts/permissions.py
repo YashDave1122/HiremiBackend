cb34281e96bcf7bcc735fb1e4ab1c43d5b603eec
pronoob828 <abdulazizahk@gmail.com>
2025-02-12 15:19:55 +0530

    removed permission verification for now

--- hiremi/accounts/permissions.py
+++ hiremi/accounts/permissions.py
@@ -3,9 +3,11 @@ from rest_framework.permissions import BasePermission
 
 class IsUser(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj == request.user  # obj is the user instance
+        return True
+        # return obj == request.user  # obj is the user instance
 
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user  # obj is owned the user instance
+        return True
+        # return obj.user == request.user  # obj is owned the user instance
