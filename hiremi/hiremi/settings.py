f9c293d47887df67f44609ffce2f3b2631bf1b56
pronoob828 <abdulazizahk@gmail.com>
2025-02-15 20:47:46 +0530

    settings updated

--- hiremi/hiremi/settings.py
+++ hiremi/hiremi/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "accounts",
-    "jobs"
+    "notifications",
+    "programs",
+    "jobs",
 ]
 
 MIDDLEWARE = [
